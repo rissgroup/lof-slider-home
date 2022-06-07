651cec709a6559b866f25216c9fa1a5cd57edcc2
Mahiuldin <mahiuldin95@gmail.com>
2022-06-07 11:59:05 +0500

    input set

--- components/bec2.jsx
+++ components/bec2.jsx
@@ -129,15 +129,15 @@ export default function bec2() {
                   <div className="mt-2">
                   <form>
                   <div className="mb-6">
-    <label for="Name" className="block mb-2 text-sm font-medium text-white">Name</label>
+    <label htmlFor="Name" className="block mb-2 text-sm font-medium text-white">Name</label>
     <input type="text" id="name" className="bg-transparent border border-[#d8aaf8] text-white text-sm rounded-lg   block w-full p-2.5  dark:text-white  " required />
   </div>
   <div className="mb-6">
-    <label for="email" className="block mb-2 text-sm font-medium text-white">Email</label>
+    <label htmlFor="email" className="block mb-2 text-sm font-medium text-white">Email</label>
     <input type="email" id="email" className="bg-transparent border border-[#d8aaf8] text-white text-sm rounded-lg   block w-full p-2.5  dark:text-white " placeholder="name@flowbite.com" required />
   </div>
   <div className="mb-6">
-    <label for="message" className="block mb-2 text-sm font-medium text-white">Message</label>
+    <label htmlFor="message" className="block mb-2 text-sm font-medium text-white">Message</label>
     <input type="text" id="message" className="bg-transparent border border-[#d8aaf8] text-white text-sm rounded-lg block w-full p-2.5  dark:text-white " required />
   </div>
   
