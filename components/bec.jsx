d4c0d197e072e18502c0a6803800d25e548925de
Mahiuldin <mahiuldin95@gmail.com>
2022-06-07 12:15:14 +0500

    useset set again

--- components/bec.jsx
+++ components/bec.jsx
@@ -7,7 +7,7 @@ import { Dialog, Transition } from '@headlessui/react'
 import { Fragment, useState } from 'react'
 
 export default function bec() {
-  const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
 
   function closeModal() {
     setIsOpen(false)
