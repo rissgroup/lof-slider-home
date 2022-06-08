4203a824d7c6f75f1fe092b04b4e179027e1c274
unknown <rissgroupdev2@gmail.com>
2022-06-08 20:13:15 +0500

    khatam ho ja

--- components/navs/Pagesnav.jsx
+++ components/navs/Pagesnav.jsx
@@ -116,8 +116,8 @@ export default function Example() {
                 
                 <div className='p-10'>
                 <div className='text-center ' >
-                  
-                  <p className='text-white text-xs font-medium hind mb-5'>The LOF platform is in development! 
+                <h1 className='text-[#d8aaf8] text-2xl font-semibold my-5 mono-font'>The LOF platform is in development! </h1>
+                  <p className='text-white text-xs font-medium hind mb-5'> 
 Fill the form below to be among the first content creators to be notified as soon as we are ready to launch!
 </p>
                 </div>
