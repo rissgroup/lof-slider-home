e17a112b0d44cb3b49355cfc8bcb922cbef11798
Mahiuldin <mahiuldin95@gmail.com>
2022-06-08 19:38:52 +0500

    backend

--- components/Contactus.jsx
+++ components/Contactus.jsx
@@ -1,8 +1,48 @@
+import React, { useState, useRef } from "react";
+
+import emailjs from "@emailjs/browser";
+import swal from 'sweetalert';
+
+
+
+
+
 export default function Contactus() {
+ 
+
+  // const form = useRef();
+
+
+  //  function sendEmail(e){
+  //   e.preventDefault();
+  //    emailjs.sendForm('service_p8mdyic', 'template_pjkm7ve', e.target, 'XXuN3CBVOAG5hVY2m' ).then(res=>{
+  //      console.log(res);
+//       swal("Email Send Successfully!", "We will get back to you soon!");
+  //      window.location.reload(false);
+
+  //    }).catch(err=>console.log(err));
+  //  }
+
+
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_p8mdyic', 'template_pjkm7ve', form.current, 'XXuN3CBVOAG5hVY2m')
+      .then((result) => {
+          console.log(result.text);
+          swal("Email Send Successfully!", "We will get back to you soon!");
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
   return (
     <>
       <div className="mt-2">
-        <form>
+        <form ref={form} onSubmit={sendEmail}>
           <div className="mb-6">
             <label
               htmlFor="Name"
@@ -13,6 +53,7 @@ export default function Contactus() {
             <input
               type="text"
               id="name"
+              name="name"
               className="bg-transparent  border-b-2 borde-x-0 border-[#d8aaf8] text-white text-sm    block w-full p-2.5  dark:text-white  outline-none "
               required
             />
@@ -27,6 +68,7 @@ export default function Contactus() {
             <input
               type="email"
               id="email"
+              name="email"
               className="bg-transparent border-b-2 border-[#d8aaf8] text-white text-sm   outline-none  block w-full p-2.5  dark:text-white "
               
               required
@@ -42,6 +84,7 @@ export default function Contactus() {
             <input
               type="text"
               id="message"
+              name="message"
               className="bg-transparent border-b-2 outline-none  border-[#d8aaf8] text-white text-sm  block w-full p-2.5  dark:text-white "
               required
             />
